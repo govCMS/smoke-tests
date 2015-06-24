bb9bb96514ceacd2e3d024af17c30cf0bf95e8cd
Sean Hamlin <sean.hamlin@acquia.com>
2015-06-24 15:52:25 +1200

    Update TEST sites.

--- sites/test.sh
+++ sites/test.sh
@@ -1,3 +1,5 @@
 https://dtodss.test-govcms.acsitefactory.com/
-https://govcms:Finance2609@govcms.test-govcms.acsitefactory.com/
-https://Asada:Th2Jebre@asada.test-govcms.acsitefactory.com/
+https://asada.test-govcms.acsitefactory.com
+https://govcms.test-govcms.acsitefactory.com/
+https://afsa.test-govcms.acsitefactory.com/
+https://communications.test-govcms.acsitefactory.com/
