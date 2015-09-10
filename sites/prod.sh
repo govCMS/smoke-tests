5b00d8b74c523e951fc849181ba75832ac9a1b54
Sean Hamlin <sean.hamlin@acquia.com>
2015-09-10 21:09:39 +1200

    Add crimtrac.

--- sites/prod.sh
+++ sites/prod.sh
@@ -1,7 +1,8 @@
+https://www.crimtrac.gov.au/
+https://www.dto.gov.au/
 https://ona:ano@ona.govcms.acsitefactory.com/
 https://casa.govcms.gov.au
 https://ministerial.govcms.gov.au/
-https://www.dto.gov.au/
 https://www.govcms.gov.au/
 https://www.asada.gov.au/
 https://www.communications.gov.au/
