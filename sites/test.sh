c47253db28fd324df6195acdc16a50b785731ac3
Paul Kilpatrick <paul.killer@gmail.com>
2016-05-30 13:45:47 +1000

    Update test.sh

--- sites/test.sh
+++ sites/test.sh
@@ -1,5 +1,2 @@
-https://dtodss.test-govcms.acsitefactory.com/
-https://asada.test-govcms.acsitefactory.com
-https://govcms.test-govcms.acsitefactory.com/
-https://afsa.test-govcms.acsitefactory.com/
-https://communications.test-govcms.acsitefactory.com/
+<insert url here>
+<insert url here>
